723f0d1d29c2db48921a9763bc6cefc34ac8e249
Asad Ali <[asad.ar451@gmail.com]>
2026-02-28 10:43:45 +0500

    add commet

--- basics/dataTyes.js
+++ basics/dataTyes.js
@@ -1,4 +1,11 @@
 "use strict"; // treat all the js code  as a newer version
 
 // alert("hello") // we are using nodejs not browser
-// alert("hello") // we are using nodejs not browser
+
+/*
+
+1: String 
+2: Number
+3: Boolean
+
+*/
